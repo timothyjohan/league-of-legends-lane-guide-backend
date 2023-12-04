bff40108603524d82ad6bcd77456dcc8f8eb08cf
lthreen <timothyjohan11@gmail.com>
2023-12-04 17:41:26 +0700

    Project initialization

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv').config().parsed;
+const { default: axios } = require('axios');
+const OpenAI = require('openai');
+const openai = new OpenAI({
+    key:dotenv.OPENAI_API_KEY
+});
+
+
+
+const app = express();
+const port = 3666;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.post('/api/chat', async (req, res) => {
+    const {champion, my_rune, enemy, enemy_rune, lane} = req.body
+    try {
+
+        const completion = await openai.chat.completions.create({
+            messages:[
+                {"role": "system", "content": `I am playing as a ${champion} ${lane} using the rune ${my_rune}, and I am fighting a ${enemy} ${lane} using the rune ${enemy_rune}, what Is the starting item i should buy? what ability do i start? what do i do to win ?  provide me per level strategy until level 6. Only answer my questions, do not give additional informations unless it's crucial`},
+                
+            ],
+            model: "gpt-3.5-turbo",
+            max_tokens: 450
+        });
+        console.log(completion);
+        return res.status(200).send(completion.choices[0].message.content)
+        
+    } catch (error) {
+        return res.status(500).send(error.error)
+        
+    }
+
+})
+
+app.get('/api/champions', async (req, res) => {
+    try {
+        const request = await axios.get(`https://ddragon.leagueoflegends.com/cdn/13.23.1/data/en_US/champion.json`)
+        const result = request.data.data
+        let champions = []
+        
+        const arrReq = Object.keys(request.data.data)
+        arrReq.forEach((champ)=>{
+            let newChamp ={
+                name: result[champ].name,
+                img: `https://ddragon.leagueoflegends.com/cdn/13.23.1/img/champion/${result[champ].image.full}`
+            }
+            champions.push(newChamp)
+        })
+        return res.status(200).send(champions)
+    } catch (error) {
+        return res.status(500).send(error)
+        
+    }
+})  
+
+
+
+app.listen(port, async ()  => { 
+    console.log(`server is running on port ${port}`);
+});
